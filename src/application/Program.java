7466d5d99e3d4b3a954548961ff735583ccef045
KauaS4ntiago <kauaandrade1806@gmail.com>
2025-08-01 09:36:36 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+	public static void main(String[] Args) {
+
+		Department d1 = new Department(1, "Books");
+		System.out.println(d1);
+	}
+}
